6d5033749c5e18d66c17993700305af151cd4b64
--get <--get>
2026-02-19 13:25:25 +0700

    Initial commit: add exercises

---
+++ exercise-1/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const userData = {
+  firstName: "Ronan",
+  lastName: "Ogor",
+  title: "PN React JS Killer",
+};
+
+export function User() {
+  return (
+    <div id="user" data-testid="user">
+      <h2>{userData.firstName} {userData.lastName}</h2>
+      <p>{userData.title}</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div id="app">
+      <h1>WELCOME !!</h1>
+      <p>PNV students are you ready to React Course ? You got this 💪</p>
+      <User />
+    </div>
+  );
+}
+
+export default App;
